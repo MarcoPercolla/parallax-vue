e3f8f05de84e07a4d2cb4ad87650ed4c9e4916fd
MarcoPercolla <percollamarco@virgilio.it>
2024-02-21 04:56:07 +0100

    aggiunge luna e minor css

--- src/components/AppComponent.vue
+++ src/components/AppComponent.vue
@@ -35,6 +35,15 @@ export default {
 <template>
     <img class="background" :style="{ 'left': 'calc(-50% - ' + xpos + 'px)', 'bottom': 'calc(-50% - ' + ypos + 'px)' }"
         src="https://images.pond5.com/simple-star-space-background-effect-023768280_prevstill.jpeg" alt="">
+    <div class="planetContainer"
+        :style="{ 'left': 'calc(-80% - ' + xpos / 2 + 'px)', 'bottom': 'calc(-50% - ' + ypos / 8 + 'px)' }">
+        <img class="planet" src="https://freepngimg.com/download/space/24616-8-space-planet-transparent.png" alt="">
+    </div>
+
+    <div class="planetContainer2"
+        :style="{ 'left': 'calc(70% - ' + xpos / 1.5 + 'px)', 'bottom': 'calc(10% + ' + ypos / 8 + 'px)' }">
+        <img class="planet" src="../../immagini/pngwing.com.png" alt="">
+    </div>
 </template>
 
 <style  lang="scss"></style>
